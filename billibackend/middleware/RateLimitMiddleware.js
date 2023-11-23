215b45e93a03abf8250b419ca91592c3b809f563
im-devposto <128308803+im-devposto@users.noreply.github.com>
2023-11-23 10:14:40 +0530

    Delete billibackend directory

--- billibackend/middleware/RateLimitMiddleware.js
+++
@@ -1,9 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // handle 100 requests within 15 min
-  message: "please try again later.",
-});
-
-export default limiter;
