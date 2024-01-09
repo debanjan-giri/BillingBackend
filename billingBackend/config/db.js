c539878897a4e97143fee640290ec22ae0617696
Debanjan Giri <128308803+debanjan-giri@users.noreply.github.com>
2024-01-09 08:28:54 +0530

    Update db.js

--- billingBackend/config/db.js
+++ billingBackend/config/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 // MongoDB Connection
 const connectDB = () => {
   try {
-    mongoose.connect(`${process.env.MONGODB_URI}`);
-    console.log(`database connected ✅`);
+    mongoose.connect(`${process.env.MONGODB_URI}`).then(()=>{console.log(`database connected ✅`)});
+    
   } catch (error) {
     console.log(`database error ❌ 
 ${error}`);
